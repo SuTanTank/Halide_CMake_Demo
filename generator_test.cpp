610432de0161a7921955208f74eeaf419156bbf1
Tan SU <sutan@insta360.com>
2019-11-03 22:38:03 +0800

    simplify cmake

--- generator_test.cpp
+++ generator_test.cpp
@@ -6,10 +6,9 @@
 int main() {
     Halide::Runtime::Buffer<uint8_t> output(640, 480);
 
-    int error1 = test_aot(output);
     int error2 = test_aot2(output);
     cv::Mat mat(480, 640, CV_8UC1, output.data());
     cv::imshow("mat", mat);
     cv::waitKey();
-    return error1;
+    return error2;
 }
